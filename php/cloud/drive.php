14e18cce0338f308fe81af7a08ce555af2d86807
Andra115 <andra15iasmina@gmail.com>
2025-05-20 17:43:39 +0300

    just some small changes

--- php/cloud/drive.php
+++ php/cloud/drive.php
@@ -6,6 +6,8 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $client = new Google_Client();
 $client->setAuthConfig(__DIR__ . '/../../driver_credentials.json');
 $client->addScope(\Google\Service\Drive::DRIVE_FILE);
+$client->setAccessType('offline');
+$client->setPrompt('consent');
 
 $authUrl = $client->createAuthUrl();
 header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
--- php/cloud/drive_oauth2callback.php
+++ php/cloud/drive_oauth2callback.php
@@ -10,6 +10,8 @@ session_start();
 $client = new Google_Client();
 $client->setAuthConfig(__DIR__ . '/../../driver_credentials.json');
 $client->addScope(Google\Service\Drive::DRIVE); 
+$client->setAccessType('offline'); 
+$client->setPrompt('consent');
 
 
 if (isset($_GET['error'])) {
@@ -19,7 +21,14 @@ if (isset($_GET['error'])) {
 }
 
 if (isset($_GET['code'])) {
+    try {
     $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+} catch (Exception $e) {
+    echo 'Exception caught: ', $e->getMessage();
+    var_dump($e);
+    exit;
+}
+
 
     if (isset($token['error'])) {
        
@@ -30,11 +39,15 @@ if (isset($_GET['code'])) {
    
     $_SESSION['access_token'] = $token['access_token'];
     if (isset($token['refresh_token'])) {
-        $_SESSION['refresh_token'] = $token['refresh_token'];
-    }
+    $_SESSION['refresh_token'] = $token['refresh_token'];
+} elseif (!isset($_SESSION['refresh_token'])) {
+    echo "No refresh token returned. Try revoking access and reauthorizing.";
+    exit;
+}
+
 
   
-    header('Location: idkyet.php');
+    header('Location: u_d_page.php');
     exit;
 } else {
    
